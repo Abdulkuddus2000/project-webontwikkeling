e69562a184c07442d92d80f03399d24b10b9fc7f
Abdul Kosimov <adkosimov@gmail.com>
2026-02-06 07:20:56 +0000

    change 1

---
+++ mugs/index.ts
@@ -0,0 +1,12 @@
+import * as rl from 'readline-sync';
+
+console.log("Welcome to the JSON data viewer!");
+console.log();
+console.log("1. View all data");
+console.log("2. Filter by ID");
+console.log("3. Exit");
+console.log();
+let choice: number = rl.questionInt("Please enter your choice: ");
+
+
+export {}
